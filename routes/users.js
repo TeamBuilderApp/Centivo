de257113a6d18f02c931e7c6159aa7c78f339d84
Christopher Peterson <teambuilderapp2@gmail.com>
2025-04-06 03:43:14 -0400

    Updated API calls

--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,18 @@ router.get('/', function(req, res, next) {
   res.send('GET user resource for Centivo Technical Challenge /users list.');
 });
 
-router.post('/', function(req, res, next) {
+router.post('/create', function(req, res, next) {
   //res.render('index', { title: 'Centivo Technical Challenge' });
   //res.render('index', { title: 'testing user 3 2 1 POST!' });
   res.send('POST: User resource for Centivo Technical Challenge. /user 3 2 1 POST!');
 });
 
-router.put('/updateuser', (req, res) => {
+router.put('/update', (req, res) => {
   res.send('PUT: User resource for Centivo Technical Challenge. /user. Todo update a User by ID.')
 });
 
-router.delete('/deleteuser', (req, res) => {
+router.delete('/delete', (req, res) => {
   res.send('DELETE: User resource for Centivo Technical Challenge. /user. Todo delete a User by ID.')
 });
 
 module.exports = router;
-  
