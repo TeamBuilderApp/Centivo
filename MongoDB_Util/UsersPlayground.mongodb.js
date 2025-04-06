0f8fc4673b8c84f9039be7041aefac34f197bcc8
Christopher Peterson <teambuilderapp2@gmail.com>
2025-04-06 04:54:47 -0400

    Added MongoDB Playground for users collection.

---
+++ MongoDB_Util/UsersPlayground.mongodb.js
@@ -0,0 +1,10 @@
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use("Centivo");
+
+// Find a document in a collection.
+db.getCollection("users").findOne({
+id:2
+});
