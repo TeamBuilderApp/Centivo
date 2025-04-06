d22653a7a642f10b3ce597baa403b44b206dcf79
Christopher Peterson <teambuilderapp2@gmail.com>
2025-04-06 08:45:55 -0400

    Added routes, installed Mongoose. Filled in POST and GET api calls.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.redirect('/users');
+router.get("/", function (req, res, next) {
+  res.redirect("/users");//Redirect to the users API page.
 });
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,68 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+const mongoose = require("mongoose");
+const Users = require("../models/users.model.js");
 
+mongoose
+  .connect(
+    "mongodb+srv://:@centivo.ejmjdpo.mongodb.net/users"
+  )
+  .then(() => {
+    console.log("Connected!");
+    //var allUsers = mongoose.getCollection("users").find({});
+    //console.log(allUsers);
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", async (req, res) => {
   var allUsers;
-MongoClient.connect(url, function(err, db) {   //here db is the client obj
-  console.log("attempting to connect to users.");
-
-  db.close();   //close method has also been moved to client obj
-});
-res.send("Connected to users db.")
-  //var allUsers = db.getCollection("users").find({});
-});
-
-router.post('/create', function(req, res, next) {
-  //res.render('index', { title: 'Centivo Technical Challenge' });
-  //res.render('index', { title: 'testing user 3 2 1 POST!' });
-  res.send('POST: User resource for Centivo Technical Challenge. /user 3 2 1 POST!');
+  try {
+    //res.send("Connected with a GET.")
+    const users = await Users.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  //res.send("Connected to users db.")
+  //
 });
 
-router.put('/update', (req, res) => {
-  res.send('PUT: User resource for Centivo Technical Challenge. /user. Todo update a User by ID.')
+router.post("/create", async (req, res) => {
+  try {
+    console.log(req.body);
+    //res.send(req.body);
+    const user = await Users.create(req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.delete('/delete', (req, res) => {
-  res.send('DELETE: User resource for Centivo Technical Challenge. /user. Todo delete a User by ID.')
+router.put("/update", async (req, res) => {
+  try {
+    console.log(req.body);
+    res.send(
+      "PUT: User resource for Centivo Technical Challenge. /user. Todo update a User by ID."
+    );
+    //res.send(req.body);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/delete", async (req, res) => {
+  try {
+    console.log(req.body);
+    res.send(
+      "DELETE: User resource for Centivo Technical Challenge. /user. Todo delete a User by ID."
+    );
+    //res.send(req.body);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
